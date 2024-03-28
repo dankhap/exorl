4c51367cc60a57250791b673c3c8c9ef2bfe1cd5
Daniel Khapun <daniel.khapun@gmail.com>
2024-03-28 15:36:49 +0200

    fix

--- run_exorl.sh
+++ run_exorl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "in bash script"
-export PATH=$PATH:/opt/conda/envs/urlb2/bin:/opt/conda/condabin
+export PATH=$PATH:/opt/conda/envs/urlb2/bin:/opt/conda/conda/bin
 echo "all arguments: $@"
 eval "$(conda shell.bash hook)"
 conda activate urlb2
 
 if [ $1 = "fine" ]; then
-	python finetune.py ${@:2}
+	python train_offline.py ${@:2}
 else 
 	python pretrain_dreamer.py ${@:2}
 fi
